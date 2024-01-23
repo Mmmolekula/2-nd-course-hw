124c44ee7cce42d3763a0f57be4f250575038b19
Maria Maltseva <mar2namebe@gmail.com>
2024-01-24 00:02:21 +0500

    Добавила js 2.5

---
+++ hw-2.5.js
@@ -0,0 +1,49 @@
+// function findMin (a, b) {
+//     if (a < b) {
+//         console.log (a)
+//     } else {
+//         console.log (b)
+//     }
+// };
+// findMin (8, 4);
+
+// function checkParity (num) {
+//     if (num % 2 === 0) {
+//         return "Число чётное";
+//     } else {
+//         return "Число нечётное";
+//     }
+// };
+// checkParity(9);
+
+// function NumberInSquare (x) {
+//     console.log (x * x);
+// };
+// NumberInSquare(5);
+
+// function NumberInSquare2 (y) {
+//     return y * y;
+// };
+// NumberInSquare2(7);
+
+// function meetUser () {
+//     let age = prompt("Сколько Вам лет?");
+//     if (age < 0) {
+//         console.log("Вы ввели неправильное значение");
+//     } else if (age <= 12) {
+//         console.log("Привет, друг!");        
+//     } else {
+//         console.log("Добро пожаловать!");
+//     }
+// }
+
+// function fewNumbers (f, e) {
+//     if (isNaN(f) || isNaN(e)) {
+//         return 'Одно или оба значения не являются числом';        
+//     } else {
+//         return f * e
+//     }
+// };
+
+
+
