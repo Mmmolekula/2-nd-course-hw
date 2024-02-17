c9071a6783a3b40a3196b96cab99680844f9a880
Maria Maltseva <mar2namebe@gmail.com>
2024-02-18 00:20:54 +0500

    Прорешала задачи

--- hw-2.7.js
+++ hw-2.7.js
@@ -17,9 +17,33 @@ console.log(Math.floor(newNumber));
 console.log(Math.ceil(newNumber));
 console.log(Math.round(newNumber));
 
-const array1 = [52, 53, 49, 77, 21, 32];
+const numbers = [52, 53, 49, 77, 21, 32];
+console.log(Math.min(...numbers));
+console.log(Math.max(...numbers));
 
+console.log(Math.floor(Math.random() * 10) + 1);
 
+function getRandomArrNumbers(nn) {
+    let result = [];
+    for (let t = 0; t < nn/2; t++) {
+        result.push(Math.floor(Math.random() * nn));
+    }
+    return result;
+};
 
-// console.log(array1Math.min());
-// console.log(Math.max());
+function randomBetween(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+console.log(new Date());
+
+let currentDate = new Date();
+currentDate.setDate(currentDate.getDate() + 73);
+console.log(currentDate);
+
+function formatDate(date) {
+    let days = ["Воскресенье", "Понедельник", "Вторник", "Среда","Суббота"];
+    let months = ["Января", "Февраля", "Марта", "Апреля", "Мая", "Июня", "Июля", "Августа", "Сентября", "Октября", "Ноября", "Декабря"];
+    return `Дата: ${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()} - это ${days[date.getDay()]} / Время: ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+};
+console.log(formatDate(new Date()));
