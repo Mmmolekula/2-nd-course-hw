5fb084a000d0421cdfeed9be2d0dcb989478871c
Maria Maltseva <mar2namebe@gmail.com>
2024-03-21 22:31:40 +0500

    Поправила ещё раз

--- Games.js
+++ Games.js
@@ -17,12 +17,12 @@ function GuessFruits () {
     fruits = fruits.sort(() => Math.random() - 0.5);
     alert(fruits);
     
-    let firstGuess = prompt("Чему равнялся первый элемент массива?").toLowerCase;
-    let lastGuess = prompt("Чему равнялся последний элемент массива?").toLowerCase;
+    let firstGuess = prompt("Чему равнялся первый элемент массива?").toLowerCase();
+    let lastGuess = prompt("Чему равнялся последний элемент массива?").toLowerCase();
 
-    if (firstGuess === fruits[0].toLowerCase && lastGuess === fruits[fruits.length - 1].toLowerCase) {
+    if (firstGuess === fruits[0].toLowerCase() && lastGuess === fruits[fruits.length - 1].toLowerCase()) {
         alert("Поздравляем! Вы угадали оба слова.");
-    } else if (firstGuess === fruits[0].toLowerCase || lastGuess === fruits[fruits.length - 1].toLowerCase) {
+    } else if (firstGuess === fruits[0].toLowerCase() || lastGuess === fruits[fruits.length - 1].toLowerCase()) {
         alert("Вы были близки к победе!");
     }
         else {
